00060cbb1b92b451ad4e80dd3f72b261e451b52f
John Biundo <johnfbiundo@gmail.com>
2019-07-04 12:13:22 -0700

    api-docs: add NestApplicationOptions and NestApplicationContextOptions

--- packages/common/interfaces/nest-application-options.interface.ts
+++ packages/common/interfaces/nest-application-options.interface.ts
@@ -2,8 +2,20 @@ import { HttpsOptions } from './external/https-options.interface';
 import { NestApplicationContextOptions } from './nest-application-context-options.interface';
 import { CorsOptions } from './external/cors-options.interface';
 
+/**
+ * @publicApi
+ */
 export interface NestApplicationOptions extends NestApplicationContextOptions {
+  /**
+   * cors options from [Express Cors package](https://github.com/expressjs/cors#configuration#options)
+   */
   cors?: boolean | CorsOptions;
+  /**
+   * use Express bodyparser
+   */
   bodyParser?: boolean;
+  /**
+   * configure HTTPS options
+   */
   httpsOptions?: HttpsOptions;
 }
