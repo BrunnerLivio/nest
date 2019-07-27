4398f2687b7232993870d10ba99a4ac3eab7c05c
John Biundo <johnfbiundo@gmail.com>
2019-07-27 16:32:40 -0700

    fix parse-int.pipe

--- packages/common/pipes/parse-int.pipe.ts
+++ packages/common/pipes/parse-int.pipe.ts
@@ -2,13 +2,6 @@ import { BadRequestException } from '../exceptions/bad-request.exception';
 import { PipeTransform } from '../interfaces/features/pipe-transform.interface';
 import { ArgumentMetadata, Injectable } from '../index';
 
-/**
- * Defines the built-in ParseInt Pipe
- *
- * @see [Built-in Pipes](https://docs.nestjs.com/pipes#built-in-pipes)
- *
- * @publicApi
- */
 @Injectable()
 export class ParseIntPipe implements PipeTransform<string> {
   /**
